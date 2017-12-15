98b77225c53edc242b31fc4081197df85f674066
SOM ON Soum <somonmel07@gmail.com>
2017-12-15 14:01:07 +0700

    initial commit

---
+++ HellowWorld.py
@@ -0,0 +1,15 @@
+print("\t\t\tFirst step")
+print("==================================================")
+name = "SOUM Somon"
+greeting = "Hello"
+# if you want to space you can add it too
+print(greeting + " " + "​" + name)
+print(2 * 4)
+print(2 * 3 + 4 - 1 - 6 / 5)
+# if you want to space you can add it too
+print(greeting + ' ' + name + '\n ')
+print("\t\t\tSecond step")
+print("==================================================")
+greeting = "Welcome"
+name = input("Please input your name here: ")
+print(greeting + ' ' + name)
